79cac7a7e4d8669ce20af64d0a8e45c5ef9f70e2
Michal Budzowski <Michal.Budzowski@sonarsource.com>
2025-09-19 16:45:18 +0200

    renamed the vb.net class and reduced it

---
+++ ConsoleApp92/Form1.vb
@@ -0,0 +1,24 @@
+﻿' VB.NET version of the provided VBScript/VB6 code
+
+Public Class Form1
+
+    ' This subroutine displays a message box when called.
+    Sub TestSub()
+        MsgBox("Code in TestSub()")
+    End Sub
+
+    ' The Form Load event handler.
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Heavily commented Sub
+        ' Heavily commented Sub
+        ' Heavily commented Sub
+        ' Heavily commented Sub
+        ' Heavily commented Sub
+        ' Heavily commented Sub
+        ' Heavily commented Sub
+        MsgBox("Code in Form_Load()")
+        TestSub()
+        MsgBox("Back in Form_Load()")
+    End Sub
+
+End Class
